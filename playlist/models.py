b378defeb25f4d74689e291722656a627b2c59be
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-09-29 21:09:45 +0900

    마이그레이션 파일 추가 24.09.29

--- playlist/models.py
+++ playlist/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.conf import settings
-
+    
 
 class Playlist(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    playlist_id = models.CharField(max_length=255) 
-    name = models.CharField(max_length=255)
+    playlist_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)  
     link = models.URLField()
     image_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
